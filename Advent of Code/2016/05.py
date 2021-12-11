6ac5f1093994c898c83ca17867abd71faef70c7b
Alexander Beck <alex@4becks.com>
2021-12-11 03:25:44 +0100

    2016/05

--- Advent of Code/2016/05.py
+++ Advent of Code/2016/05.py
@@ -1,18 +1,40 @@
 
-# Started
-# Finished
+# Started 03:04
+# Finished 03:25
+
+import hashlib
 
 f = open('2016/05.txt', 'r')
 inputString = f.read()
 
+inputString = 'ffykfhsq'
+
+#inputString = 'abc'
+
 def partOne(i):
-    pass
+    x = 0
+    password = ''
+    while len(password) < 8:
+        hashable = i + str(x)
+        hashed = hashlib.md5(hashable.encode())
+        hexhash = hashed.hexdigest()
+        if(hexhash[:5] == '00000'):
+            password += str(hexhash[5])
+        x += 1
+    return password
 
 def partTwo(i):
-    pass
+    x = 0
+    password = 'xxxxxxxx'
+    while 'x' in password:
+        hashable = i + str(x)
+        hashed = hashlib.md5(hashable.encode())
+        hexhash = hashed.hexdigest()
+        if hexhash[:5] == '00000' and hexhash[5] in ('0', '1', '2', '3', '4', '5', '6', '7') and password[int(hexhash[5])] == 'x':
+            password = password[:int(hexhash[5])] + hexhash[6] + password[int(hexhash[5]) + 1:]
+        x += 1
+    return password
 
-inputArray = inputString.splitlines()
+print(partOne(inputString))
 
-print(partOne(inputArray))
-
-print(partTwo(inputArray))
+print(partTwo(inputString))
