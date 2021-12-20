e3388e76b67464aa88a6c467d20c2b628365f3bb
Alexander <alex@4becks.com>
2021-12-20 18:07:59 +0100

    2021/15 start

--- Advent of Code/2021/15.py
+++ Advent of Code/2021/15.py
@@ -5,6 +5,17 @@
 f = open('2021/15.txt', 'r')
 inputString = f.read()
 
+#inputString = """1163751742
+#1381373672
+#2136511328
+#3694931569
+#7463417111
+#1319128137
+#1359912421
+#3125421639
+#1293138521
+#2311944581"""
+
 def partOne(i):
     pass
 
@@ -13,6 +24,15 @@ def partTwo(i):
 
 inputArray = inputString.splitlines()
 
-print(partOne(inputArray))
+grid = [[int(j) for j in x] for x in inputArray]
 
-print(partTwo(inputArray))
+for g in grid:
+    for c in g:
+        print(f"{c} ", end = "")
+    print()
+
+csv = open('')
+
+print(partOne(grid))
+
+print(partTwo(grid))
