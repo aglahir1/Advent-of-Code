4564b827ae9ab6b436cd0073a41ff53776d614c5
Alexander <alex@4becks.com>
2022-07-21 16:32:31 +0200

    2016/08+09

--- Advent of Code/2016/08.py
+++ Advent of Code/2016/08.py
@@ -2,17 +2,76 @@
 # Started
 # Finished
 
+import copy
+
 f = open('2016/08.txt', 'r')
 inputString = f.read()
 
-def partOne(i):
-    pass
+def print2DArray(arr):
+    for r in arr:
+        for l in r:
+            print(end=l)
+        print()
+    print()
+
+def rect(a,b,screen):
+    work = copy.deepcopy(screen)
+    for i in range(b):
+        for j in range(a):
+            work[i][j] = '#'
+    return work
+
+def rotateCol(col,amount,screen):
+    work = copy.deepcopy(screen)
+    for x in range(len(work)):
+        work[(x+amount)%len(work)][col] = screen[x][col]
+    return work
+
+def rotateRow(row,amount,screen):
+    work = copy.deepcopy(screen)
+    for x in range(len(work[0])):
+        work[row][(x+amount)%len(work[0])] = screen[row][x]
+    return work
+
+def countLitPixels(screen):
+    count=0
+    for y in screen:
+        for x in y:
+            if x == '#': count += 1
+    return count
+
+def parse(instruction, screen):
+    work = copy.deepcopy(screen)
+    instruction = instruction.split()
+    command = instruction.pop(0)
+    if command == 'rect':
+        a, b = map(int,instruction.pop(0).split('x'))
+        work = rect(a, b, work)
+    else:
+        axisType = instruction.pop(0)
+        axis = int(instruction.pop(0)[2:])
+        amount = int(instruction.pop())
+        if axisType == 'row':
+            work = rotateRow(axis, amount, work)
+        else:
+            work = rotateCol(axis, amount, work)
+    return work
+
+def partOne(instructions, screen):
+    for i in instructions:
+        screen = parse(i, screen)
+    print2DArray(screen)
+    return countLitPixels(screen)
+
 
 def partTwo(i):
     pass
 
 inputArray = inputString.splitlines()
 
-print(partOne(inputArray))
+screen = [['.']*50 for _ in range(6)]
+
+
+print(partOne(inputArray, screen))
 
 print(partTwo(inputArray))
--- Advent of Code/2016/09.py
+++ Advent of Code/2016/09.py
@@ -5,14 +5,34 @@
 f = open('2016/09.txt', 'r')
 inputString = f.read()
 
-def partOne(i):
-    pass
+def deflatedLength(string):
+    if string.find('(') == -1: return len(string)
+    count = 0
+    while string.find('(') != -1:
+        pre, post = string.split('(', 1)
+        count += len(pre)
+        markerString, post = post.split(')', 1)
+        marker = list(map(int,markerString.split('x')))
+        count += marker[1] * deflatedLength(post[:marker[0]])
+        string = post[marker[0]:]
+    return count
 
-def partTwo(i):
-    pass
 
-inputArray = inputString.splitlines()
+def partOne(inputString):
+    deflatedSize = 0
+    while inputString.find('(') != -1:
+        pre, post = inputString.split('(', 1)
+        deflatedSize += len(pre)
+        markerString, post = post.split(')', 1)
+        marker = list(map(int,markerString.split('x')))
+        #deflatedSize += deflatedLength(marker, post[:marker[0]], False)
+        inputString = post[marker[0]:]
+    return deflatedSize
 
-print(partOne(inputArray))
+def partTwo(inputString):
+    return deflatedLength(inputString)
 
-print(partTwo(inputArray))
+
+#print(partOne(inputString))
+
+print(partTwo(inputString))
