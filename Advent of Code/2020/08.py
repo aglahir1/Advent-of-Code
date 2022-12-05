e7a5cf459d07a6d08fea1d7565078ec2eb4a3779
Alexander <alex@4becks.com>
2022-12-05 18:53:54 +0100

    2020/08

--- Advent of Code/2020/08.py
+++ Advent of Code/2020/08.py
@@ -5,11 +5,61 @@
 f = open('2020/08.txt', 'r')
 inputString = f.read()
 
+#inputString = """nop +0
+#acc +1
+#jmp +4
+#acc +3
+#jmp -3
+#acc -99
+#acc +1
+#jmp -4
+#acc +6"""
+
+def run(i):
+    visited = []
+    pos = 0
+    acc = 0
+    while pos not in visited:
+        visited += [pos]
+        instruction = i[pos].split()
+        if instruction[0] == 'acc':
+            pos += 1
+            acc += int(instruction[1])
+        elif instruction[0] == 'jmp':
+            pos += int(instruction[1])
+        elif instruction[0] == 'nop':
+            pos += 1
+    return acc
+
+def checkRun(i):
+    visited = []
+    pos = 0
+    acc = 0
+    while pos not in visited and 0 <= pos < len(i):
+        visited += [pos]
+        instruction = i[pos].split()
+        if instruction[0] == 'acc':
+            pos += 1
+            acc += int(instruction[1])
+        elif instruction[0] == 'jmp':
+            pos += int(instruction[1])
+        elif instruction[0] == 'nop':
+            pos += 1
+    return [len(i) <= pos or pos < 0, acc]
+
+
+
 def partOne(i):
-    pass
+    return run(i)
+
 
 def partTwo(i):
-    pass
+    for x in range(len(i)):
+        if i[x].split()[0] == 'nop':
+            test = checkRun(i[:x] + ['jmp' + i[x][3:]] + i[x+1:])
+        elif i[x].split()[0] == 'jmp':
+            test = checkRun(i[:x] + ['nop' + i[x][3:]] + i[x+1:])
+        if test[0]: return test[1]
 
 inputArray = inputString.splitlines()
 
