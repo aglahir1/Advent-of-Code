bb72aac99df0a7da904f23a895e87e26268ee976
Alexander Beck <alex@4becks.com>
2023-11-24 20:39:28 +0100

    2017/02

--- Advent of Code/2017/02.py
+++ Advent of Code/2017/02.py
@@ -2,16 +2,35 @@
 # Started
 # Finished
 
+
 f = open('2017/02.txt', 'r')
 inputString = f.read()
+#inputString = """5 9 2 8
+#9 4 7 3
+#3 8 6 5"""
 
 def partOne(i):
-    pass
+    differences: list = []
+    for row in i:
+        differences.append(max(row) - min(row))
+    return sum(differences)
 
 def partTwo(i):
-    pass
+    divisions: list = []
+    for row in i:
+        row = sorted(row, reverse = True)
+        divisions.append(findMultiple(row))
+    return sum(divisions)
 
-inputArray = inputString.splitlines()
+
+def findMultiple(row:list):
+        for j in range(len(row)):
+            for y in range(j + 1,len(row)):
+                if row[j] % row[y] == 0:
+                    return int(row[j] / row[y])
+
+
+inputArray = [[int(y) for y in x.split()] for x in inputString.splitlines()]
 
 print(partOne(inputArray))
 
