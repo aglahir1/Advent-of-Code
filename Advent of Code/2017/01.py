dd7e4ceb7341b948989d45937963d4f1349dab3b
Alexander Beck <alex@4becks.com>
2023-11-24 20:21:17 +0100

    2017/01

--- Advent of Code/2017/01.py
+++ Advent of Code/2017/01.py
@@ -2,16 +2,30 @@
 # Started
 # Finished
 
+
+
 f = open('2017/01.txt', 'r')
-inputString = f.read()
+inputString: str = f.read()
+
+#inputString = '1122'
 
 def partOne(i):
-    pass
+    numbers: list = []
+    for pointer in range(len(i)):
+        num1: int = int(i[pointer])
+        num2: int = int(i[(pointer + 1) % len(i)])
+        if num1 == num2: numbers.append(num1)
+    return sum(numbers)
 
 def partTwo(i):
-    pass
+    numbers: list = []
+    for pointer in range(len(i)):
+        num1: int = int(i[pointer])
+        num2: int = int(i[(pointer + int(len(i)/2)) % len(i)])
+        if num1 == num2: numbers.append(num1)
+    return sum(numbers)
 
-inputArray = inputString.splitlines()
+inputArray: list = list(inputString)
 
 print(partOne(inputArray))
 
