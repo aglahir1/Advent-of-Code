cb88813efcf5d3305b7c5ac1ca95142ac1724215
Alexander Beck <alex@4becks.com>
2023-12-03 00:39:07 +0100

    2017/04

--- Advent of Code/2017/04.py
+++ Advent of Code/2017/04.py
@@ -6,10 +6,19 @@ f = open('2017/04.txt', 'r')
 inputString = f.read()
 
 def partOne(i):
-    pass
+    runningSum = 0
+    for line in i:
+        passphrase = line.split()
+        if len(passphrase) == len(set(passphrase)): runningSum += 1
+    return runningSum
 
 def partTwo(i):
-    pass
+    runningSum = 0
+    for line in i:
+        passphrase = line.split()
+        rearranged = [''.join(sorted(list(x))) for x in passphrase]
+        if len(passphrase) == len(set(rearranged)): runningSum += 1
+    return runningSum
 
 inputArray = inputString.splitlines()
 
