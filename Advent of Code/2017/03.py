4df9eaeee054620bab6c4ec1ee68a1751cb8383c
Alexander Beck <alex@4becks.com>
2023-12-02 21:04:07 +0100

    2017/03

--- Advent of Code/2017/03.py
+++ Advent of Code/2017/03.py
@@ -2,17 +2,78 @@
 # Started
 # Finished
 
-f = open('2017/03.txt', 'r')
-inputString = f.read()
+from collections import defaultdict
+
+
+inputInt = 325489
 
 def partOne(i):
-    pass
+    currentNum = 1
+    x = 0
+    y = 0
+    for circleNumber in range(1, 287):
+        x += 1
+        currentNum += 1
+        for rightside in range(y + 1, circleNumber + 1):
+            y = rightside
+            currentNum += 1
+            if currentNum == i:
+                return abs(y) + abs(x)
+        for topside in range(x - 1, -circleNumber - 1, -1):
+            x = topside
+            currentNum += 1
+            if currentNum == i:
+                return abs(y) + abs(x)
+        for leftside in range(y - 1, -circleNumber - 1, -1):
+            y = leftside
+            currentNum += 1
+            if currentNum == i:
+                return abs(y) + abs(x)
+        for bottomside in range(x + 1, circleNumber + 1):
+            x = bottomside
+            currentNum += 1
+            if currentNum == i:
+                return abs(y) + abs(x)
+
+def getAdjacents(x, y, spiral):
+    return spiral.get(x - 1, {}).get(y, 0) + spiral.get(x - 1, {}).get(y - 1, 0) + spiral.get(x - 1, {}).get(y + 1, 0) + spiral.get(x + 1, {}).get(y, 0) +spiral.get(x + 1, {}).get(y - 1, 0) + spiral.get(x + 1, {}).get(y + 1, 0)+ spiral.get(x, {}).get(y - 1, 0) + spiral.get(x, {}).get(y + 1, 0)
 
 def partTwo(i):
-    pass
+    spiral = defaultdict(dict)
+    spiral[0][0] = 1
+    x = 0
+    y = 0
+    for circleNumber in range(1, 287):
+        x += 1
+        num = getAdjacents(x, y, spiral)
+        if num > i:
+            return num
+        spiral[x][y] = num
+        for rightside in range(y + 1, circleNumber + 1):
+            y = rightside
+            num = getAdjacents(x, y, spiral)
+            if num > i:
+                return num
+            spiral[x][y] = num
+        for topside in range(x - 1, -circleNumber - 1, -1):
+            x = topside
+            num = getAdjacents(x, y, spiral)
+            if num > i:
+                return num
+            spiral[x][y] = num
+        for leftside in range(y - 1, -circleNumber - 1, -1):
+            y = leftside
+            num = getAdjacents(x, y, spiral)
+            if num > i:
+                return num
+            spiral[x][y] = num
+        for bottomside in range(x + 1, circleNumber + 1):
+            x = bottomside
+            num = getAdjacents(x, y, spiral)
+            if num > i:
+                return num
+            spiral[x][y] = num
 
-inputArray = inputString.splitlines()
+print(partOne(inputInt))
 
-print(partOne(inputArray))
-
-print(partTwo(inputArray))
+print(partTwo(inputInt))
