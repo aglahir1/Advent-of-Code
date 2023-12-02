210617adaf2049ed3e69a5e4294d9a25d55d7d4b
Alexander Beck <alex@4becks.com>
2023-12-03 00:46:08 +0100

    2017/05

--- Advent of Code/2017/05.py
+++ Advent of Code/2017/05.py
@@ -2,17 +2,42 @@
 # Started
 # Finished
 
+from copy import deepcopy
+
+
 f = open('2017/05.txt', 'r')
 inputString = f.read()
+#inputString = """0
+#3
+#0
+#1
+#-3"""
 
 def partOne(i):
-    pass
+    pointer = 0
+    steps = 0
+    while pointer >= 0 and pointer < len(i):
+        instruction = i[pointer]
+        i[pointer] += 1
+        pointer += instruction
+        steps += 1
+    return steps
 
 def partTwo(i):
-    pass
+    pointer = 0
+    steps = 0
+    while pointer >= 0 and pointer < len(i):
+        instruction = i[pointer]
+        if instruction >= 3:
+            i[pointer] -= 1
+        else:
+            i[pointer] += 1
+        pointer += instruction
+        steps += 1
+    return steps
 
-inputArray = inputString.splitlines()
+inputArray = [int(x) for x in inputString.splitlines()]
 
-print(partOne(inputArray))
+print(partOne(deepcopy(inputArray)))
 
-print(partTwo(inputArray))
+print(partTwo(deepcopy(inputArray)))
