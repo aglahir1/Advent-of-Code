587012e5ed3be0a0b2309dbd53298ceccec78adc
Alexander <alex@4becks.com>
2023-12-05 17:18:23 +0100

    2017/09

--- Advent of Code/2017/09.py
+++ Advent of Code/2017/09.py
@@ -5,14 +5,42 @@
 f = open('2017/09.txt', 'r')
 inputString = f.read()
 
+#inputString = '{{{},{},{{}}}}'
+
+def cleanoutgarbage(i):
+    cleaned = ''
+    garbaged = 0
+    gFlag = False
+    throw = False
+    for c in i:
+        if throw:
+            throw = False
+            continue
+        if gFlag:
+            if c not in '!>':
+                garbaged += 1
+                continue
+            if c == '!':
+                throw = True
+                continue
+            gFlag = False
+            continue
+        if c == '<':
+            gFlag = True
+            continue
+        cleaned += c
+    return (cleaned, garbaged)
+
 def partOne(i):
-    pass
+    i = cleanoutgarbage(i)
+    level = 0
+    runningSum = 0
+    for c in i[0]:
+        if c == '{':
+            level += 1
+        if c == '}':
+            runningSum += level
+            level -= 1
+    return (runningSum,i[1])
 
-def partTwo(i):
-    pass
-
-inputArray = inputString.splitlines()
-
-print(partOne(inputArray))
-
-print(partTwo(inputArray))
+print(partOne(inputString))
