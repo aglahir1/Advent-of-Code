5ffa4d2ee408356175d004618bffcf1cb910a988
Alexander <alex@4becks.com>
2023-12-05 10:49:46 +0100

    2023/04

--- Advent of Code/2023/04.py
+++ Advent of Code/2023/04.py
@@ -5,11 +5,37 @@
 f = open('2023/04.txt', 'r')
 inputString = f.read()
 
+#inputString = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+#Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+#Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+#Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+#Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+#Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
+
 def partOne(i):
-    pass
+    runningSum = 0
+    for line in i:
+        card = line.split(': ')[1].split(' | ')
+        winners = set(card[0].split())
+        card = set(card[1].split())
+        winningNums = winners.intersection(card)
+        if len(winningNums) > 0:
+            runningSum += 2 ** (len(winningNums) - 1)
+    return runningSum
+
 
 def partTwo(i):
-    pass
+    cards = {r: 1 for r in range(len(i))}
+    for r, line in enumerate(i):
+        card = line.split(': ')[1].split(' | ')
+        winners = set(card[0].split())
+        card = set(card[1].split())
+        winningNums = winners.intersection(card)
+        if len(winningNums) > 0:
+            for extra in range(len(winningNums)):
+                cards[r + extra + 1] += cards[r]
+    return sum(cards.values())
+
 
 inputArray = inputString.splitlines()
 
