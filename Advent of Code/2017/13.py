2095aa1890df2e6d583b7937c62ec8a9e61892ab
Alexander Beck <alex@4becks.com>
2023-12-07 04:14:34 +0100

    2017/13

--- Advent of Code/2017/13.py
+++ Advent of Code/2017/13.py
@@ -5,13 +5,27 @@
 f = open('2017/13.txt', 'r')
 inputString = f.read()
 
+# inputString = """0: 3
+# 1: 2
+# 4: 4
+# 6: 4"""
+
 def partOne(i):
-    pass
+    runningSum = 0
+    for line in i:
+        d = (line[1] - 1) * 2
+        if line[0] % d == 0:
+            runningSum += line[0] * line[1]
+    return runningSum
 
 def partTwo(i):
-    pass
+    delay = 0
+    while True:
+        if all([(line[0] + delay) % ((line[1] - 1) * 2) > 0 for line in i]):
+            return delay
+        delay += 1
 
-inputArray = inputString.splitlines()
+inputArray = [[int(j) for j in x.split(': ')] for x in inputString.splitlines()]
 
 print(partOne(inputArray))
 
