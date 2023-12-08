693054ee9ed06eb949fe85104a783e5e80fc7876
Alexander <alex@4becks.com>
2023-12-08 19:20:19 +0100

    2023/08 part 1

--- Advent of Code/2023/08.py
+++ Advent of Code/2023/08.py
@@ -2,17 +2,55 @@
 # Started
 # Finished
 
+import re
+
 f = open('2023/08.txt', 'r')
 inputString = f.read()
 
+#inputString = """RL
+
+#AAA = (BBB, CCC)
+#BBB = (DDD, EEE)
+#CCC = (ZZZ, GGG)
+#DDD = (DDD, DDD)
+#EEE = (EEE, EEE)
+#GGG = (GGG, GGG)
+#ZZZ = (ZZZ, ZZZ)"""
+
+class Node:
+    def __init__(self, name: str):
+        self.name = name
+
+    def directions(self, left: 'Node', right: 'Node'):
+        self.left = left
+        self.right = right
+
 def partOne(i):
-    pass
+    pointer = nodes['AAA']
+    count = 0
+    while True:
+        for c in i:
+            count += 1
+            if c == 'R':
+                pointer = pointer.right
+            elif c == 'L':
+                pointer = pointer.left
+            if pointer.name == 'ZZZ':
+                return count
 
 def partTwo(i):
     pass
 
-inputArray = inputString.splitlines()
+inputArray = inputString.split('\n\n')
 
-print(partOne(inputArray))
 
-print(partTwo(inputArray))
+nodes = {x.split()[0]: Node(x.split()[0]) for x in inputArray[1].splitlines()}
+
+for n in inputArray[1].splitlines():
+    result = re.search(r'\((.{3}), (.{3})\)', n)
+    nodes[n.split()[0]].directions(nodes[result.group(1)], nodes[result.group(2)])
+
+
+print(partOne(inputArray[0]))
+
+print(partTwo(inputArray[0]))
