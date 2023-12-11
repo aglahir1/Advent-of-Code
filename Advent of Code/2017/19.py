d2b4a4212d40c356fc2ac529460e4bedb3e7aca9
Alexander Beck <alex@4becks.com>
2023-12-11 03:31:41 +0100

    2017/19

--- Advent of Code/2017/19.py
+++ Advent of Code/2017/19.py
@@ -5,14 +5,41 @@
 f = open('2017/19.txt', 'r')
 inputString = f.read()
 
-def partOne(i):
-    pass
+def partOne(i: list[str]):
+    pY = 1
+    pX = i[1].index('|')
+    heading = 'd'
+    letters = []
+    count = 0
+    while 0 <= pY < len(i) and 0 <= pX < len(i[0]):
+        count += 1
+        match heading:
+            case 'd':
+                pY += 1
+            case 'u':
+                pY -= 1
+            case 'l':
+                pX -= 1
+            case 'r':
+                pX += 1
+        if i[pY][pX] not in '|-+ ':
+            letters.append(i[pY][pX])
+            print(letters)
+            continue
+        if i[pY][pX] == ' ':
+            break
+        if i[pY][pX] == '+':
+            if i[pY + 1][pX] == '|' and heading not in 'ud':
+                heading = 'd'
+            elif i[pY - 1][pX] == '|' and heading not in 'ud':
+                heading = 'u'
+            elif i[pY][pX + 1] == '-' and heading not in 'lr':
+                heading = 'r'
+            elif i[pY][pX - 1] == '-' and heading not in 'lr':
+                heading = 'l'
+    return [''.join(letters), count]
 
-def partTwo(i):
-    pass
-
-inputArray = inputString.splitlines()
+inputArray = [' ' + x + ' ' for x in inputString.splitlines()]
+inputArray = [' '*len(inputArray)] + inputArray + [' '*len(inputArray)]
 
 print(partOne(inputArray))
-
-print(partTwo(inputArray))
