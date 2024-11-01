4ed0b8f79e34337396fb96c181715a0d4775dbf9
Alexander Beck <alex@4becks.com>
2024-11-01 03:02:15 +0100

    2016/12

--- Advent of Code/2016/12.py
+++ Advent of Code/2016/12.py
@@ -5,11 +5,44 @@
 f = open('2016/12.txt', 'r')
 inputString = f.read()
 
+# inputString = """cpy 41 a
+# inc a
+# inc a
+# dec a
+# jnz a 2
+# dec a"""
+
+def executeInst(assembunny: dict) -> dict:
+    inst = assembunny['instructions'][assembunny['pointer']]
+    inst = inst.split(' ')
+    if inst[0] == 'cpy':
+        if inst[1].isnumeric():
+            assembunny[inst[2]] = int(inst[1])
+        else:
+            assembunny[inst[2]] = assembunny[inst[1]]
+    elif inst[0] == 'inc':
+        assembunny[inst[1]] += 1
+    elif inst[0] == 'dec':
+        assembunny[inst[1]] -= 1
+    elif inst[0] == 'jnz':
+        if inst[1].isnumeric() and inst[1] != '0':
+            assembunny['pointer'] += int(inst[2]) - 1
+        elif assembunny[inst[1]] != 0:
+            assembunny['pointer'] += int(inst[2]) - 1
+    assembunny['pointer'] += 1
+    return assembunny
+
 def partOne(i):
-    pass
+    assembunny = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'pointer': 0, 'instructions': i}
+    while 0 <= assembunny['pointer'] < len(assembunny['instructions']):
+        assembunny = executeInst(assembunny)
+    return assembunny
 
 def partTwo(i):
-    pass
+    assembunny = {'a': 0, 'b': 0, 'c': 1, 'd': 0, 'pointer': 0, 'instructions': i}
+    while 0 <= assembunny['pointer'] < len(assembunny['instructions']):
+        assembunny = executeInst(assembunny)
+    return assembunny
 
 inputArray = inputString.splitlines()
 
