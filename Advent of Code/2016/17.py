2b419b0e6a445e31e42747f59463740ffcb1181b
Alexander Beck <alex@4becks.com>
2024-11-17 00:04:25 +0100

    2016/17

--- Advent of Code/2016/17.py
+++ Advent of Code/2016/17.py
@@ -2,17 +2,84 @@
 # Started
 # Finished
 
-f = open('2016/17.txt', 'r')
-inputString = f.read()
+from copy import deepcopy
+from hashlib import md5
 
-def partOne(i):
-    pass
+OPEN = 'bcdef'
 
-def partTwo(i):
-    pass
+passcode = 'vwbaicqe'
 
-inputArray = inputString.splitlines()
+# passcode = 'ulqzkmiv'
+# passcode = 'kglvqrro'
+# passcode = 'ihgpwlah'
 
-print(partOne(inputArray))
+def getDoors(position: tuple[int,int], route: str) -> str:
+    hash = md5(str.encode(passcode + route)).hexdigest()
+    availableDoors = ''
+    if position[1] != 0 and hash[0] in OPEN:
+        availableDoors += 'U'
+    if position[1] != 3 and hash[1] in OPEN:
+        availableDoors += 'D'
+    if position[0] != 0 and hash[2] in OPEN:
+        availableDoors += 'L'
+    if position[0] != 3 and hash[3] in OPEN:
+        availableDoors += 'R'
+    return availableDoors
 
-print(partTwo(inputArray))
+
+def partOne():
+    runningPaths = [((0,0), '')]
+    steps = 0
+    while True:
+        newRunningPaths = []
+        for p in runningPaths:
+            doors = getDoors(p[0], p[1])
+            if doors == '': continue
+            for d in doors:
+                match d:
+                    case 'U':
+                        newPos = (p[0][0], p[0][1] - 1)
+                    case 'D':
+                        newPos = (p[0][0], p[0][1] + 1)
+                    case 'L':
+                        newPos = (p[0][0] - 1, p[0][1])
+                    case 'R':
+                        newPos = (p[0][0] + 1, p[0][1])
+                if newPos == (3,3): return p[1] + d
+                newRunningPaths.append((newPos, p[1] + d))
+        steps += 1
+        print(steps)
+        runningPaths = deepcopy(newRunningPaths)
+    
+
+def partTwo():
+    runningPaths = [((0,0), '')]
+    steps = 0
+    currentLongest = 0
+    while True:
+        newRunningPaths = []
+        for p in runningPaths:
+            doors = getDoors(p[0], p[1])
+            if doors == '': continue
+            for d in doors:
+                match d:
+                    case 'U':
+                        newPos = (p[0][0], p[0][1] - 1)
+                    case 'D':
+                        newPos = (p[0][0], p[0][1] + 1)
+                    case 'L':
+                        newPos = (p[0][0] - 1, p[0][1])
+                    case 'R':
+                        newPos = (p[0][0] + 1, p[0][1])
+                if newPos == (3,3):
+                    currentLongest = len(p[1]) + 1
+                    continue
+                newRunningPaths.append((newPos, p[1] + d))
+        steps += 1
+        if len(newRunningPaths) == 0: return currentLongest
+        runningPaths = deepcopy(newRunningPaths)
+
+
+print(partOne())
+
+print(partTwo())
