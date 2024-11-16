7560cd23c743f34227d6efa946ec9ff6898df17b
Alexander Beck <alex@4becks.com>
2024-11-16 01:03:06 +0100

    2016/14

--- Advent of Code/2016/14.py
+++ Advent of Code/2016/14.py
@@ -2,17 +2,70 @@
 # Started
 # Finished
 
-f = open('2016/14.txt', 'r')
-inputString = f.read()
+from hashlib import md5
 
-def partOne(i):
-    pass
+inputString = 'zpqevtbw'
 
-def partTwo(i):
-    pass
+# inputString = 'abc'
 
-inputArray = inputString.splitlines()
+def checkquins(hash: str) -> set[str]:
+    runningc: str = ''
+    tally: int = 0
+    quins: set[str] = set()
+    for c in hash:
+        if c == runningc:
+            tally += 1
+            if tally == 5:
+                quins.add(c)
+        else:
+            tally = 1
+            runningc = c
+    return quins
 
-print(partOne(inputArray))
+def checktrips(hash: str) -> str:
+    runningc: str = ''
+    tally: int = 0
+    for c in hash:
+        if c == runningc:
+            tally += 1
+            if tally == 3:
+                return c
+        else:
+            tally = 1
+            runningc = c
+    return ''
+            
+def fullhash(input: str, part: int) -> str:
+    input = md5(str.encode(input)).hexdigest()
+    if part == 2:
+        for _ in range(2016):
+            input = md5(str.encode(input)).hexdigest()
+    return input
 
-print(partTwo(inputArray))
+def parts(i, part: int):
+    hashes = {}
+    index = 0
+    keys = []
+    end = 'not found'
+    while True:
+        hash = fullhash(str(i) + str(index), part)
+        quins = checkquins(hash)
+        trips = checktrips(hash)
+        if len(quins) > 0 or len(trips) > 0:
+            hashes[index] = (hash, trips, quins)
+        if len(quins) > 0:
+            for x in range(index - 1000, index):
+                if x in hashes and x not  in keys:
+                    if hashes[x][1] in quins:
+                        keys.append(x)
+                        if len(keys) == 64 and end == 'not found': end = x + 1000
+        index += 1
+        if index == end:
+            keys.sort()
+            return keys[63]
+
+
+
+print(parts(inputString, 1))
+
+print(parts(inputString, 2))
