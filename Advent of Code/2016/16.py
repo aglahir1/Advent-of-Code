72a5a5380c2e3a1ab95fd77cc62a764cf0ea937b
Alexander Beck <alex@4becks.com>
2024-11-16 01:59:44 +0100

    2016/16

--- Advent of Code/2016/16.py
+++ Advent of Code/2016/16.py
@@ -2,17 +2,39 @@
 # Started
 # Finished
 
-f = open('2016/16.txt', 'r')
-inputString = f.read()
 
-def partOne(i):
-    pass
+input = '10111011111001111'
+disksize = 272
 
-def partTwo(i):
-    pass
 
-inputArray = inputString.splitlines()
+def step(a: str) -> str:
+    length = len(a)
+    b = a[::-1]
+    b = (1 << length) - 1 - int(b, 2)
+    return a + '0' + str(bin(b))[2:].zfill(length)
 
-print(partOne(inputArray))
+def checksum(data: str) -> str:
+    result = ''
+    for x in zip(data[0::2], data[1::2]):
+        if x[0] == x[1]:
+            result += '1'
+        else: 
+            result += '0'
+    return result
 
-print(partTwo(inputArray))
+
+def partOne(i,d):
+    data = i
+    while len(data) < d:
+        data = step(data)
+    result = checksum(data[:d])
+    while len(result) % 2 == 0:
+        result = checksum(result)
+    return result
+
+
+
+
+print(partOne(input, 272))
+
+print(partOne(input, 35651584))
