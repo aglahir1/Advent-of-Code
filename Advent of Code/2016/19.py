0c947b31b8f9b4c34e7b7d94c07156c5b0d724b9
Alexander Beck <alex@4becks.com>
2024-11-23 00:27:14 +0100

    2016/19 part 2 super fuckin slow

--- Advent of Code/2016/19.py
+++ Advent of Code/2016/19.py
@@ -2,17 +2,41 @@
 # Started
 # Finished
 
-f = open('2016/19.txt', 'r')
-inputString = f.read()
+from math import floor
 
+
+pInput = 3005290
+
+# pInput = 5
+
+class Circle:
+    def __init__(self, size: int):
+        self.seats = size
+        self.elves = list(range(1, size + 1))
+        self.stealer = 0
+    
 def partOne(i):
-    pass
+    circle = list(range(i))
+    while len(circle) > 1:
+        if len(circle) % 2 == 0:
+            circle = circle[::2]
+        else:
+            circle = [circle[-1]] + circle[:-1:2]
+    return circle[0] + 1
 
 def partTwo(i):
-    pass
+    circle = Circle(i)
+    cycle = 0
+    while circle.seats > 1:
+        kickee = (circle.stealer + floor(circle.seats / 2)) % circle.seats
+        circle.elves.pop(kickee)
+        circle.seats -= 1
+        circle.stealer = (circle.stealer + [0, 1][kickee > circle.stealer]) % circle.seats
+        cycle += 1
+        if cycle % 3000 == 0: print(cycle / 30000, '/ 100')
+    return circle.elves[0]
+        
+        
+print(partOne(pInput))
 
-inputArray = inputString.splitlines()
-
-print(partOne(inputArray))
-
-print(partTwo(inputArray))
+print(partTwo(pInput))
