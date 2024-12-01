d15100a8cfc47fb4bf742f0e1a99d134ffa1d50d
Alexander Beck <alex@4becks.com>
2024-12-01 06:09:51 +0100

    2024/01 wooooo aoc!!!

--- Advent of Code/2024/01.py
+++ Advent of Code/2024/01.py
@@ -2,16 +2,37 @@
 # Started
 # Finished
 
+from copy import deepcopy
+
+
 f = open('2024/01.txt', 'r')
 inputString = f.read()
 
+# inputString = """3   4
+# 4   3
+# 2   5
+# 1   3
+# 3   9
+# 3   3"""
+
 def partOne(i):
-    pass
+    l1 = deepcopy(i[0])
+    l2 = deepcopy(i[1])
+    l1.sort()
+    l2.sort()
+    return sum([abs(a[0] - a[1]) for a in zip(l1, l2)])
 
 def partTwo(i):
-    pass
+    l1 = deepcopy(i[0])
+    l2 = deepcopy(i[1])
+    runningsum = 0
+    for x in l1:
+        runningsum += x * l2.count(x)
+    return runningsum
 
-inputArray = inputString.splitlines()
+lines = inputString.splitlines()
+
+inputArray = [[int(x.split()[0]) for x in lines], [int(x.split()[1]) for x in lines]]
 
 print(partOne(inputArray))
 
