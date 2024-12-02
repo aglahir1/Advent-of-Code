0dbb281b41de873e754aa5f42316eef3c8e9ad91
Alexander <alex@4becks.com>
2024-12-02 11:54:20 +0100

    2024/02

--- Advent of Code/2024/02.py
+++ Advent of Code/2024/02.py
@@ -2,16 +2,42 @@
 # Started
 # Finished
 
+from copy import deepcopy
+
+
 f = open('2024/02.txt', 'r')
 inputString = f.read()
 
+#inputString = """7 6 4 2 1
+#1 2 7 8 9
+#9 7 6 2 1
+#1 3 2 4 5
+#8 6 4 4 1
+#1 3 6 7 9"""
+
 def partOne(i):
-    pass
+    validreports = 0
+    for r in i:
+        if (all(r[j] < r[j+1] for j in range(len(r) - 1)) or all(r[j] > r[j+1] for j in range(len(r) - 1))) and all(abs(r[j] - r[j+1]) < 4 for j in range(len(r) - 1)): validreports += 1
+    return validreports
+
 
 def partTwo(i):
-    pass
+    validreports = 0
+    for r in i:
+        if (all(r[j] < r[j+1] for j in range(len(r) - 1)) or all(r[j] > r[j+1] for j in range(len(r) - 1))) and all(abs(r[j] - r[j+1]) < 4 for j in range(len(r) - 1)): 
+            validreports += 1
+            continue
+        for x in range(len(r)):
+            temp = deepcopy(r)
+            temp.pop(x)
+            if (all(temp[j] < temp[j+1] for j in range(len(temp) - 1)) or all(temp[j] > temp[j+1] for j in range(len(temp) - 1))) and all(abs(temp[j] - temp[j+1]) < 4 for j in range(len(temp) - 1)):
+                validreports += 1
+                break
+    return validreports
 
-inputArray = inputString.splitlines()
+reports = [x.split() for x in inputString.splitlines()]
+inputArray = [[int(j) for j in x] for x in reports]
 
 print(partOne(inputArray))
 
