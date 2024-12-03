0fb641c07549ee1373a75eaba64008317e2cbd9a
Alexander Beck <alex@4becks.com>
2024-12-03 22:40:37 +0100

    2024/03

--- Advent of Code/2024/03.py
+++ Advent of Code/2024/03.py
@@ -2,17 +2,27 @@
 # Started
 # Finished
 
+import re
+
 f = open('2024/03.txt', 'r')
 inputString = f.read()
 
+# inputString = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5
+# inputString = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()don't()?mul(8,5))"
+
+mulregex = re.compile(r'mul\((\d+),(\d+)\)')
+dontregex = re.compile(r"don't\(\).*?do\(\)")
+finaldont = re.compile(r"don't\(\).*$")
+
 def partOne(i):
-    pass
+    return sum([int(x[0]) * int(x[1]) for x in re.findall(mulregex, i)])
 
 def partTwo(i):
-    pass
+    i = re.sub(dontregex, '', i)
+    i = re.sub(finaldont, '', i)
+    return sum([int(x[0]) * int(x[1]) for x in re.findall(mulregex, i)])
 
-inputArray = inputString.splitlines()
 
-print(partOne(inputArray))
+print(partOne(inputString))
 
-print(partTwo(inputArray))
+print(partTwo(inputString))
