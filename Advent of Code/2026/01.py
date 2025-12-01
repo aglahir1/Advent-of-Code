428d533de2d3b110e53c153ceb88cfaae3e0a06e
Alexander Beck <alex@4becks.com>
2025-12-01 22:02:02 +0100

    2026/01

--- Advent of Code/2026/01.py
+++ Advent of Code/2026/01.py
@@ -5,11 +5,48 @@
 f = open('2026/01.txt', 'r')
 inputString = f.read()
 
+# inputString = """L68
+# L30
+# R48
+# L5
+# R60
+# L55
+# L1
+# L99
+# R14
+# L82"""
+
 def partOne(i):
-    pass
+    pos = 50
+    count = 0
+    for instruction in i:
+        if instruction[0] == 'L':
+            m = -1
+        else:
+            m = 1
+        pos += m * int(instruction[1:])
+        if pos % 100 == 0: count += 1
+    return count
 
 def partTwo(i):
-    pass
+    pos = 50
+    count = 0
+    for instruction in i:
+        if instruction[0] == 'L':
+            m = -1
+            if pos == 0: pos = 100
+        else:
+            m = 1
+        pos += m * int(instruction[1:])
+        if 0 < pos < 100:
+            continue
+        if pos < 1:
+            count += -(pos - 100) // 100
+        elif pos > 99:
+            count += pos // 100
+        pos %= 100
+    return count
+
 
 inputArray = inputString.splitlines()
 
